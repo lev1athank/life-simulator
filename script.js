83147a7c36aafd402509ffcba16fad36241ae0d6
lev1athank <nik.parh.googl@gmail.com>
2023-03-16 01:30:27 +0500

    before the final

--- script.js
+++ script.js
@@ -20,7 +20,18 @@ const draw = (x, y)=> ctx.fillRect(x, y, pix, pix)
 
 draw(160, 160)
 draw(170, 160)
+draw(170, 150)
 draw(180, 160)
+
+draw(100, 100)
+draw(110, 100)
+draw(120, 100)
+
+
+draw(220, 160)
+draw(230, 160)
+draw(240, 160)
+draw(230, 150)
 
 ctx.stroke();
 
@@ -32,20 +43,30 @@ function dead_AND_life() {
     kill.forEach(([x, y])=> ctx.clearRect(x, y, pix, pix))
     revive.forEach(([x, y])=> draw(x, y))
     ctx.stroke();
+    revive = []
+    kill = []
 }
+
 
 
 
 
 function check(x, y) {
+
     data = ctx.getImageData(x-pix, y-pix, pix*3, pix*3)
     //ctx.strokeRect(x-pix, y-pix, pix*3, pix*3); //del
     let near = data.data.filter(el=>el == 255).length / 100
-    if (near == 1 || near > 3) kill.push([x,y])
-    else if(near == 3) revive.push([x,y])
+
+    
+    if(ctx.getImageData(x+pix/2, y+pix/2, pix/2, pix/2).data.filter(el=>el == 255).length) {
+        near-=1
+        if(near <= 1 || near > 3) kill.push([x,y])
+    }else{
+        if(near == 3) revive.push([x,y])
+    }
 }
 
-const Start = ()=>setTimeout(()=>{
+const Start = setInterval(()=>{
     const w = conves.width
     const h = conves.height
     for (let x = 0; x < w; x+=pix) {
@@ -54,7 +75,8 @@ const Start = ()=>setTimeout(()=>{
         }
     }
     dead_AND_life()
-}, 1000)
+    
+}, 300)
 
 
-document.addEventListener('keypress', ()=>Start())
+// document.addEventListener('keypress', ()=>Start())
