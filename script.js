8f1d778ccb440085d7ade9e0633c191acf45152c
lev1athank <nik.parh.googl@gmail.com>
2023-03-15 23:49:08 +0500

    почти все

--- script.js
+++ script.js
@@ -2,36 +2,59 @@ const conves = document.querySelector('.conv')
 
 const ctx = conves.getContext('2d', { willReadFrequently: true })
 
-const pix = 20
+const pix = 10
 
-ctx.beginPath();
+
 ctx.lineWidth = 2
-for (let x = pix; x < conves.width; x+=pix) {
-    ctx.moveTo(x, 0)
-    ctx.lineTo(x, conves.height)
-    ctx.moveTo(0, x)
-    ctx.lineTo(conves.width, x)
-}
-function draw(x, y) {
-    ctx.fillStyle = 'blue'
-    ctx.lineWidth = 1
-    ctx.rect(x, y, pix, pix)
-    ctx.fill()
-}
+// for (let x = pix; x < conves.width; x+=pix) {
+//     ctx.moveTo(x, 0)
+//     ctx.lineTo(x, conves.height)
+//     ctx.moveTo(0, x)
+//     ctx.lineTo(conves.width, x)
+// }
 
-draw(20, 20)
+
+const draw = (x, y)=> ctx.fillRect(x, y, pix, pix)
+
+
+
+draw(160, 160)
+draw(170, 160)
+draw(180, 160)
+
 ctx.stroke();
 
-for (let x = 0; x < conves.width; x+=pix) {
-    for (let y = 0; y < conves.height; y+=pix) {
-        let data = ctx.getImageData(x, y, pix, pix)
-        for (let index = 0; index < data.data.length; index++) {
-            console.log(data.data[index] === 0);
-            
+let revive = []
+let kill = []
+
+function dead_AND_life() {
+    ctx.beginPath();
+    kill.forEach(([x, y])=> ctx.clearRect(x, y, pix, pix))
+    revive.forEach(([x, y])=> draw(x, y))
+    ctx.stroke();
+}
+
+
+
+
+function check(x, y) {
+    data = ctx.getImageData(x-pix, y-pix, pix*3, pix*3)
+    //ctx.strokeRect(x-pix, y-pix, pix*3, pix*3); //del
+    let near = data.data.filter(el=>el == 255).length / 100
+    if (near == 1 || near > 3) kill.push([x,y])
+    else if(near == 3) revive.push([x,y])
+}
+
+const Start = ()=>setTimeout(()=>{
+    const w = conves.width
+    const h = conves.height
+    for (let x = 0; x < w; x+=pix) {
+        for (let y = 0; y < h; y+=pix) {
+            check(x, y)
         }
-        
-}}
+    }
+    dead_AND_life()
+}, 1000)
 
 
-
-
+document.addEventListener('keypress', ()=>Start())
